cfd31178fb315c0089047502a9e48d89efbba686
Julio Souza <visoboti@gmail.com>
2023-01-18 11:06:58 -0300

    meus exercicios React

---
+++ 01-react-estado/src/components/panel/panel.js
@@ -0,0 +1,31 @@
+import { Component } from 'react'
+import './panel.css'
+
+class Panel extends Component{
+    constructor(){
+        super()
+        this.state = {
+            title: 'Título do painel'
+        }
+    }
+
+    render(){        
+        return(
+            
+            <section className="panel" onClick={
+                
+               () => {            
+                 console.log(this.state)  
+                    this.setState(
+                        {title: 'Título novo'}
+                       
+                    )
+                }
+            }>
+                <h2>{this.state.title}</h2>
+            </section>
+        )
+    }
+}
+
+export default Panel
